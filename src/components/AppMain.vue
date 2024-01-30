13d3b7b3bf5eab14069bf2071b0091fe0a143c19
AndreaParrini <andrea.parrini26@gmail.com>
2024-01-31 00:19:12 +0100

    Add new componets AppReferalLink and link to appmain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,11 @@
 <script>
+import AppReferalLink from './AppReferalLink.vue';
+
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+    components: {
+        AppReferalLink
+    }
 }
 </script>
 
@@ -11,30 +16,7 @@ export default {
                 --&gt; Content goes here &lt;--
             </div>
         </section>
-        <section class="link">
-            <div class="container">
-                <div class="col">
-                    <img src="/img/buy-comics-digital-comics.png" alt="">
-                    <a href="#">Digital Comics</a>
-                </div>
-                <div class="col">
-                    <img src="/img/buy-comics-merchandise.png" alt="">
-                    <a href="#">Dc Merchandise</a>
-                </div>
-                <div class="col">
-                    <img src="/img/buy-comics-subscriptions.png" alt="">
-                    <a href="#">Subscription</a>
-                </div>
-                <div class="col">
-                    <img src="/img/buy-comics-shop-locator.png" alt="">
-                    <a href="#">Comic Shop locator</a>
-                </div>
-                <div class="col">
-                    <img src="/img/buy-dc-power-visa.svg" alt="">
-                    <a href="#">Dc Power Visa</a>
-                </div>
-            </div>
-        </section>
+        <AppReferalLink></AppReferalLink>
     </main>
 </template>
 
@@ -46,35 +28,5 @@ main .content {
     font-size: 1.5rem;
     font-weight: 700;
     padding: 3rem 0;
-}
-
-main .link {
-    background-color: var(--dc-primary);
-    padding: 3rem 0;
-    position: relative;
-
-    & .container {
-        display: flex;
-    }
-
-    & .col {
-        display: flex;
-
-        & img {
-            max-height: 4rem;
-            max-width: 4rem;
-        }
-
-        & a {
-            text-decoration: none;
-            color: var(--dc-light);
-            display: flex;
-            align-items: center;
-            margin-left: 0.5rem;
-            font-weight: 500;
-            font-size: 0.9rem;
-            text-transform: uppercase;
-        }
-    }
 }
 </style>
---
+++ src/components/AppReferalLink.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    name: 'AppReferalLink'
+}
+</script>
+
+<template>
+    <section class="link">
+        <div class="container">
+            <div class="col">
+                <img src="/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">Digital Comics</a>
+            </div>
+            <div class="col">
+                <img src="/img/buy-comics-merchandise.png" alt="">
+                <a href="#">Dc Merchandise</a>
+            </div>
+            <div class="col">
+                <img src="/img/buy-comics-subscriptions.png" alt="">
+                <a href="#">Subscription</a>
+            </div>
+            <div class="col">
+                <img src="/img/buy-comics-shop-locator.png" alt="">
+                <a href="#">Comic Shop locator</a>
+            </div>
+            <div class="col">
+                <img src="/img/buy-dc-power-visa.svg" alt="">
+                <a href="#">Dc Power Visa</a>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<style scoped>
+.link {
+    background-color: var(--dc-primary);
+    padding: 3rem 0;
+    position: relative;
+
+    & .container {
+        display: flex;
+    }
+
+    & .col {
+        display: flex;
+
+        & img {
+            max-height: 4rem;
+            max-width: 4rem;
+        }
+
+        & a {
+            text-decoration: none;
+            color: var(--dc-light);
+            display: flex;
+            align-items: center;
+            margin-left: 0.5rem;
+            font-weight: 500;
+            font-size: 0.9rem;
+            text-transform: uppercase;
+        }
+    }
+}
+</style>
