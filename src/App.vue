3ff958934120a0f4ebf7687fc3e7b60f1667f110
AndreaParrini <andrea.parrini26@gmail.com>
2024-01-31 00:10:48 +0100

    add new components AppFooter and link in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script >
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    AppFooter
   }
 }
 </script>
@@ -14,144 +16,7 @@ export default {
 <template>
   <AppHeader></AppHeader>
   <AppMain></AppMain>
-
-  <footer>
-    <section class="section_links">
-      <div class="container">
-        <div class="links">
-          <div class="col">
-            <h3>Dc Comics</h3>
-            <ul>
-              <li><a href="#">Characters</a></li>
-              <li><a href="#">Comics</a></li>
-              <li><a href="#">Movies</a></li>
-              <li><a href="#">TV</a></li>
-              <li><a href="#">Games</a></li>
-              <li><a href="#">Videos</a></li>
-              <li><a href="#">News</a></li>
-            </ul>
-            <h3>Shop</h3>
-            <ul>
-              <li><a href="#">Shop DC</a></li>
-              <li><a href="#">Shop DC Collectibles</a></li>
-            </ul>
-          </div>
-          <div class="col">
-            <h3>Dc</h3>
-            <ul>
-              <li><a href="#">Term Of Use</a></li>
-              <li><a href="#">Privacy policy (New)</a></li>
-              <li><a href="#">Ad Choices</a></li>
-              <li><a href="#">Advertising</a></li>
-              <li><a href="#">Jobs</a></li>
-              <li><a href="#">Subscriptions</a></li>
-              <li><a href="#">Talent Workshops</a></li>
-              <li><a href="#">CPSC Certificates</a></li>
-              <li><a href="#">Ratings</a></li>
-              <li><a href="#">Shop Help</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
-          <div class="col">
-            <h3>Sites</h3>
-            <ul>
-              <li><a href="#">Dc</a></li>
-              <li><a href="#">MAD Magazine</a></li>
-              <li><a href="#">Movies</a></li>
-              <li><a href="#">DC Kids</a></li>
-              <li><a href="#">DC Universe</a></li>
-              <li><a href="#">DC Power Visa</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="thumb">
-          <img src="/img/dc-logo-bg.png" alt="">
-        </div>
-      </div>
-    </section>
-
-    <section class="section_follow">
-      <div class="container">
-        <div class="sign_up">
-          <div class="btn">
-            <a href="#">sign-up now!</a>
-          </div>
-        </div>
-        <div class="follow_us">
-
-        </div>
-      </div>
-
-    </section>
-  </footer>
+  <AppFooter></AppFooter>
 </template>
 
-<style>
-.section_links {
-  background-image: url(/img/footer-bg.jpg);
-  background-size: cover;
-
-  & .container {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  & .links {
-    display: flex;
-  }
-
-  & img {
-    height: 400px;
-    overflow: hidden;
-  }
-
-  & h3 {
-    text-transform: uppercase;
-    color: var(--dc-light);
-    margin-top: 1.5rem;
-    margin-bottom: 1rem;
-    font-size: larger;
-    font-weight: 700;
-  }
-
-  & li {
-    list-style: none;
-
-    & a {
-      text-decoration: none;
-      color: var(--dc-gray);
-      font-size: small;
-      font-weight: 200;
-    }
-  }
-
-  & img {
-    height: 600px;
-    margin-top: -100px;
-    margin-bottom: -100px;
-  }
-
-}
-
-
-.section_follow {
-  background-color: var(--dc-footer);
-  position: relative;
-  padding: 2rem;
-
-  & .container {
-    display: flex;
-  }
-
-  & .btn {
-    padding: 1rem;
-    border: 2px solid var(--dc-primary);
-
-    & a {
-      text-transform: uppercase;
-      color: var(--dc-light);
-      text-decoration: none;
-    }
-  }
-}
-</style>
+<style></style>
